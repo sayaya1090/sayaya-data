c80326f4535c511c694c9e2cd97af8961d41dca4
sayaya <sayaya1090@hotmail.com>
2023-06-04 11:55:04 +0900

    feat: .

--- src/main/java/net/sayaya/data/Observable.java
+++ src/main/java/net/sayaya/data/Observable.java
@@ -2,8 +2,13 @@ package net.sayaya.data;
 
 import net.sayaya.ui.event.HasValueChangeHandlers;
 
+import java.util.function.Function;
+
 public interface Observable<T> extends HasValueChangeHandlers<T> {
     void fireValueChangeEvent();
+    static <A, B> void wire(Observable<A> source, Function<A, B> mapper, ObservableSingle<B> sink) {
+        source.onValueChange(evt->sink.value(mapper.apply(evt.value())));
+    }
     interface ObservableSingle<T> extends Observable<T> {
         void value(T value);
     }
